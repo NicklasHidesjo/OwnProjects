64e7602e850d409d229fb63a564e2b30891f7260
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2021-02-09 21:04:45 +0100

    Working CubeMaze Creation
    Needs to be done (refactor and rewrite the code making it better), split the mazeCreation into several scripts)
    Fix so that odd shapes (rectangles) can be created.

--- MazeRunner/Assets/Node.cs
+++ MazeRunner/Assets/Node.cs
@@ -9,6 +9,11 @@ public class Node : MonoBehaviour
     [SerializeField] GameObject southWall = null;
     [SerializeField] GameObject westWall = null;
 
+    [SerializeField] GameObject thiccNorthWall = null;
+    [SerializeField] GameObject thiccEastWall = null;
+    [SerializeField] GameObject thiccSouthWall = null;
+    [SerializeField] GameObject thiccWestWall = null;
+ 
     Vector2Int coordinates = new Vector2Int(0, 0);
     public Vector2Int Coordinates { get { return coordinates; } set { coordinates = value; } }
 
@@ -18,15 +23,50 @@ public class Node : MonoBehaviour
     Vector2Int exploredFrom = new Vector2Int(0,0);
     public Vector2Int ExploredFrom { get { return exploredFrom; } set { exploredFrom = value; } }
 
-    public void DestroyWall(Vector2 direction)
+    public void DestroyWall(Vector2Int direction)
     {
-        if (direction == Vector2.up)
+        if (direction == Vector2Int.up)
+        {
             northWall.SetActive(false);
-        else if(direction == Vector2.right)
+            thiccNorthWall.SetActive(false);
+        }
+        else if (direction == Vector2Int.right)
+        {
             eastWall.SetActive(false);
-        else if(direction == Vector2.down)
+            thiccEastWall.SetActive(false);
+        }
+        else if (direction == Vector2Int.down)
+        {
+            thiccSouthWall.SetActive(false);
             southWall.SetActive(false);
+        }
         else
+        {
             westWall.SetActive(false);
+            thiccWestWall.SetActive(false);
+        }
+    }
+    public void TurnOnThiccWall(Vector2Int direction)
+    {
+        if (direction == Vector2Int.up)
+        {
+            northWall.SetActive(false);
+            thiccNorthWall.SetActive(true);
+        }
+        else if (direction == Vector2Int.right)
+        {
+            eastWall.SetActive(false);
+            thiccEastWall.SetActive(true);
+        }
+        else if (direction == Vector2Int.down)
+        {
+            southWall.SetActive(false);
+            thiccSouthWall.SetActive(true);
+        }
+        else
+        {
+            westWall.SetActive(false);
+            thiccWestWall.SetActive(true);
+        }
     }
 }
